bc1605d0d927a826195022be08991b411bcab59b
Hygor Ferreira <hygor09@gmail.com>
2024-10-07 10:24:51 -0300

    att

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, addDoc } from 'firebase/firestore' // Import Firestore functions
 
-// Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: 'AIzaSyCV3L3z4p9UUsnik5H1KLIZLh2kShGyyLo',
-  authDomain: 'rueda-qrcode.firebaseapp.com',
-  projectId: 'rueda-qrcode',
-  storageBucket: 'rueda-qrcode.appspot.com',
-  messagingSenderId: '351567393506',
-  appId: '1:351567393506:web:0f962a51b6d17558e3a4bb',
-  measurementId: 'G-PGLM3LXN7B'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
